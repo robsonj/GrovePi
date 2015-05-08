c4d494fe2a3f8f10c86d7c148482acecd0da387d
Exadon <Exadon@gmail.com>
2015-05-08 15:34:13 -0400

    Cleanup

--- GrovePi/Sensors/ChainableRgbLed.cs
+++ GrovePi/Sensors/ChainableRgbLed.cs
@@ -13,6 +13,13 @@ namespace GrovePi.Sensors
 
     public class ChainableRgbLed : IChainableRgbLed
     {
+        public const byte StoreColorCommandAddress = 90;
+        public const byte InitialiseCommandAddress = 91;
+        public const byte TestCommandAddress = 92;
+        public const byte SetPatternCommandAddress = 93;
+        public const byte SetModuloCommandAddress = 94;
+        public const byte SetLevelCommmandAddress = 95;
+
         private readonly GrovePi _device;
         private readonly Pin _pin;
 
@@ -25,42 +32,38 @@ namespace GrovePi.Sensors
 
         public void Initialise(int NumberOfLeds)
         {
-            var buffer = new byte[] { CommandAddress.Initialise, (byte)_pin, (byte)NumberOfLeds, Constants.Unused };
+            var buffer = new byte[] { InitialiseCommandAddress, (byte)_pin, (byte)NumberOfLeds, Constants.Unused };
             _device.DirectAccess.Write(buffer);
         }
 
         public void StoreColor(int red, int green, int blue)
         {
-            var buffer = new byte[] { CommandAddress.StoreColor, (byte)red, (byte)green, (byte)blue };
+            var buffer = new byte[] { StoreColorCommandAddress, (byte)red, (byte)green, (byte)blue };
             _device.DirectAccess.Write(buffer);
         }
 
         public void Test(int NumberOfLeds, int TestColor)
         {
-            var buffer = new byte[] { CommandAddress.Test, (byte)_pin, (byte)NumberOfLeds, (byte)TestColor };
+            var buffer = new byte[] { TestCommandAddress, (byte)_pin, (byte)NumberOfLeds, (byte)TestColor };
             _device.DirectAccess.Write(buffer);
         }
 
         public void SetPattern(int Pattern, int Led)
         {
-            var buffer = new byte[] { CommandAddress.SetPattern, (byte)_pin, (byte)Pattern, (byte)Led };
+            var buffer = new byte[] { SetPatternCommandAddress, (byte)_pin, (byte)Pattern, (byte)Led };
             _device.DirectAccess.Write(buffer);
         }
 
         public void Mudulo(int Offset, int Divisor)
         {
-            var buffer = new byte[] { CommandAddress.SetPattern, (byte)_pin, (byte)Offset, (byte)Divisor };
+            var buffer = new byte[] { SetModuloCommandAddress, (byte)_pin, (byte)Offset, (byte)Divisor };
             _device.DirectAccess.Write(buffer);
         }
-    }
 
-    class CommandAddress
-    {
-        public const byte StoreColor = 90;
-        public const byte Initialise = 91;
-        public const byte Test = 92;
-        public const byte SetPattern = 93;
-        public const byte SetModulo = 94;
-        public const byte SetLevel = 95;
+        public void SetLevel(int Level, bool Reverse)
+        {
+            var buffer = new byte[] { (byte)Level, (byte)_pin, (byte)Level, Reverse ? (byte)1 : (byte)0 };
+            _device.DirectAccess.Write(buffer);
+        }
     }
 }
