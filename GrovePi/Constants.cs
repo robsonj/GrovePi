ba651cb5825c168040fcc0ad3d8d6b79149c042d
Exadon <Exadon@gmail.com>
2015-05-09 11:02:30 -0400

    Added RgbLcdDisplay

--- GrovePi/Constants.cs
+++ GrovePi/Constants.cs
@@ -5,5 +5,7 @@
         public const byte Unused = 0;
         public const byte GroveVcc = 5;
         public const byte AdcVoltage = 5;
+        public const int GroveRgpLcdMaxLength = 16;
+        public const int GroveRgpLcdRows = 2;
     }
 }
