e6a0f10ad61692b7d090ab56bd8852d94b16ae1f
Exadon <Exadon@gmail.com>
2015-05-14 20:20:49 -0400

    Added authors

--- Driver/SimpleDriver.cs
+++ Driver/SimpleDriver.cs
@@ -4,6 +4,11 @@ using GrovePi.Sensors;
 
 namespace Driver
 {
+    /// <summary>
+    /// Library Authors:
+    /// John https://github.com/robsonj
+    /// Paul Binder Jr https://github.com/Exadon http://www.PaulBinderJr.com
+    /// </summary>
     public sealed class SimpleDriver : IBackgroundTask
     {
         private readonly IBuildGroveDevices _deviceFactory = DeviceFactory.Build;
@@ -17,6 +22,10 @@ namespace Driver
             //    .MeasureInCentimeters();
 
             //_deviceFactory.BuildBuzzer(Pin.DigitalPin2).ChangeState(SensorStatus.On);
+
+            //        var tempInCelcius = _deviceFactory
+            //.BuildTemperatureAndHumiditySensor(Pin.DigitalPin2, Model.OnePointTwo)
+            //.TemperatureInCelcius();
 
             //var level = _deviceFactory.BuildLightSensor(Pin.DigitalPin3)
             //    .SensorValue();
